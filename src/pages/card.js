f30dbbd42ec8d285e3bc9e0b62c279f021471bea
Tanya Prakash <jhatanya16aug@gmail.com>
2021-02-09 10:47:37 +0530

    initial changes done

---
+++ src/pages/card.js
@@ -0,0 +1,33 @@
+// import ReactCardFlip from 'react-card-flip';
+// import React from 'react';
+// class Card extends React.Component
+//  {
+//     constructor() {
+//       super();
+//         this.state = {
+//         isFlipped: false
+//       };
+//       this.handleClick = this.handleClick.bind(this);
+//     }
+   
+//     handleClick(e) {
+//       e.preventDefault();
+//       this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+//     }
+   
+//     render() {
+//       return (
+//         <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+//             This is the front of the card.
+//             <button onClick={this.handleClick}>Click to flip</button>
+        
+   
+         
+//             This is the back of the card.
+//             <button onClick={this.handleClick}>Click to flip</button>
+         
+//         </ReactCardFlip>
+//       )
+//     }
+// }
+// export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,110 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Datas from '../Datas.json';
+// import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import "../css/index.css";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <body>
+    <h1 className="heading">Maths Department Seminar</h1>
+    <div className="time">Feb 10-11.Online.Free</div>
+     <div className="desc">
+     <p>
+
+     Join us on March 2-3, 2021 for this free virtual event.
+      Connect with Gatsby community members from all over the world, 
+      advance your skills with free workshops, and hear the latest
+     announcements from the Gatsby team. 
+      Claim your spot!
+     </p>
+   </div>
+   {/* <div>
+     <Card/>
+   </div> */}
+    <section className="talks">
+      <div className="talk-head">
+        <h3>February 10,2021 </h3>
+        <h1 className="guest-lect">Guest Lectures</h1>
+        <p>Local Times Shown Below</p>
+      </div>
+     
+    </section>
+   
+    <ul className="guest-Lectures">
+    {Datas.guests.map((item, i) => (
+       <section className="talks">
+   <li key={i} >
+   <a href="#" >
+   <p style={{color:"white"}} className="pdate">{item.start_time} - {item.end_time}</p>
+    <p className="pname">{item.name}</p>
+     <p className="pdesc">{item.description}</p>
+     <p className="ptopic">Topic: {item.topic}</p>
+     </a>
+      </li>
+      </section>
+
+  ))}
+      </ul>
+ <section className="sponsors">
+   <h1 className="sponcer-head">Sponsors</h1>
+   <h3 className="sponsor-name">Organized By</h3>
+   <div className="img-sponsors">
+     <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Birla_Institute_of_Technology_Mesra.png/220px-Birla_Institute_of_Technology_Mesra.png" style={{height:90,width:90}}/>
+   <h3>Maths-Department</h3>
+   </div>
+   <h3 className="sponsor-name">Sponsored By</h3>
+   <div className="img-sponsors">
+     <img src="https://mondaymorning.nitrkl.ac.in/uploads/post/TEQIP.jpg" style={{height:90,width:200}}/>
+   <h3>TEQIP</h3>
+   </div>
+
+ </section>
+
+
+<MDBFooter color="cyan" className="font-small darken-3 pt-0">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="12" className="py-5">
+            <div className="mb-5 flex-center">
+              <a className="fb-ic">
+                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="tw-ic">
+                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="gplus-ic">
+                <i className="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="li-ic">
+                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="ins-ic">
+                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="pin-ic">
+                <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+              </a>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a href="#">   Birla_Institute_of_Technology_Mesra-Department of Mathematics </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  </body>
 )
 
-export default IndexPage
+export default IndexPage;
